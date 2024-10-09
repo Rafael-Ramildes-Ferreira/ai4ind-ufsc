adf9f47ae124ddc35d9056f83d8d5e69516f23b4
Jomi Fred Hubner <jomifred@gmail.com>
2024-10-09 10:13:11 -0300

    add browser service

--- wot/run.sh
+++ wot/run.sh
@@ -1,1 +1,5 @@
-docker run -ti --rm -v ".:/app" -v "./gradle-cache:/root/.gradle" --network=ai4ind-ufsc_ai4industry -p 3272:3272 -w /app jomifred/jacamo:1.2 wot.jcm
+docker run -ti --rm -v ".:/app" \
+   -v "./gradle-cache:/root/.gradle" \
+   --network=ai4ind-ufsc_ai4industry -p 3272:3272 \
+   -w /app \
+   jomifred/jacamo:1.2 wot.jcm
